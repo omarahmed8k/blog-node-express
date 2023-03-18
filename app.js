db0c0dc75582ae28a0ec52f26dfa325bb21a049f
Omar Abdelrahman <omarahmed8k@gmail.com>
2023-03-18 20:11:09 +0200

    Cors

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
+const cors = require('cors')
 const morgan = require('morgan');
 const blogRoutes = require('./src/routes/blogRoutes');
 const pagesRoutes = require('./src/routes/pagesRoutes');
+require('dotenv').config();
 
 const app = express();
-const port = 3000;
+const port = 8000;
 
-const uri = process.env.mongoDB;
+const uri = process.env.MONGO_DB;
 
 const mongoose = require('mongoose');
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -17,6 +19,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(morgan('dev'));
+app.use(cors());
 
 app.use(pagesRoutes);
 app.use('/blogs', blogRoutes);
